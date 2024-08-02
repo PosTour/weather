b783b5ecacf074bdb3dcf3f77d8a541e8ce90b1f
posTour <dimidov496@gmail.com>
2024-08-02 23:48:12 +0300

    feat: создан сервис для отправки сообщений боту

---
+++ src/main/java/ru/spring/weather/service/KafkaSenderService.java
@@ -0,0 +1,16 @@
+package ru.spring.weather.service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaSenderService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaSenderService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendToBot(String message) {
+        kafkaTemplate.send("bot", message);
+    }
+}
