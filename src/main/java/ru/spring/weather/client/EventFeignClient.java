38762a4af11b9076e84bc7cbfd572b3ab56c5449
posTour <dimidov496@gmail.com>
2024-08-08 14:30:12 +0300

    feat: добавлены возможности получения всех явлений пользователя и удаления явлений; обновлены dto явлений

--- src/main/java/ru/spring/weather/client/EventFeignClient.java
+++ src/main/java/ru/spring/weather/client/EventFeignClient.java
@@ -1,7 +1,6 @@
 package ru.spring.weather.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.spring.weather.dto.ForecastDto;
 
