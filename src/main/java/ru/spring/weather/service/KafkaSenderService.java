ef68187a7dca2cae57611754c8e9ff003255e414
posTour <dimidov496@gmail.com>
2024-08-11 21:42:21 +0300

    fix: kafka

--- src/main/java/ru/spring/weather/service/KafkaSenderService.java
+++ src/main/java/ru/spring/weather/service/KafkaSenderService.java
@@ -1,5 +1,6 @@
 package ru.spring.weather.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +9,14 @@ public class KafkaSenderService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    @Value("${spring.kafka.producer.topic}")
+    private String topic;
+
     public KafkaSenderService(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendToBot(String message) {
-        kafkaTemplate.send("bot", message);
+        kafkaTemplate.send(topic, message);
     }
 }
